bb14f14602110ba33d6293e0d2f286308e2013b5
Eric D. Helms <ericdhelms@gmail.com>
2020-12-22 08:11:21 -0500

    Split out test generation to conftest.py

--- tests/test_pages.py
+++ tests/test_pages.py
@@ -1,7 +1,6 @@
 # pylint: disable=redefined-outer-name
 from __future__ import print_function
 
-from collections import namedtuple
 from typing import Union, KeysView, List
 from urllib.parse import ParseResult, urlparse, parse_qs, urlencode
 
@@ -11,8 +10,6 @@ import requests
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-
-User = namedtuple('User', ['username', 'password', 'name'])
 
 PAGES = [
     '/',
@@ -67,39 +64,6 @@ def chrome_options(chrome_options):
 def session_capabilities(session_capabilities):
     session_capabilities['acceptInsecureCerts'] = True
     return session_capabilities
-
-
-def _user(variables):
-    return User(variables.get('username', 'admin'), variables.get('password', 'changeme'),
-                variables.get('name', 'Admin User'))
-
-
-@pytest.fixture(scope='session')
-def user(variables):
-    return _user(variables)
-
-
-def pytest_generate_tests(metafunc):
-    if 'url' in metafunc.fixturenames:
-        base_url = metafunc.config.option.base_url
-        assert base_url
-
-        # mimic the variables fixture from pytest-variables
-        user_obj = _user(metafunc.config._variables)  # pylint: disable=protected-access
-
-        response = requests.get(f'{base_url}/menu', auth=(user_obj.username, user_obj.password),
-                                verify=False)
-        if response.status_code == 404:
-            # Menu is only available since Foreman 2.0
-            pages = [base_url + page for page in PAGES]
-        else:
-            assert response
-            pages = [pytest.param(base_url + page['url'], id=page['name'])
-                     for page in response.json()
-                     # logout has an error "Cannot read property 'icon' of null"
-                     if page['url'] != '/users/logout']
-
-        metafunc.parametrize('url', pages)
 
 
 def filtered_url_query(url: str, allowed_query_params: Union[List, KeysView]) -> ParseResult:
