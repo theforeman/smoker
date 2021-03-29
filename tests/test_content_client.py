28fbc0ab8c829d35ccb3b8b0607a67deb514719b
Eric D. Helms <ericdhelms@gmail.com>
2021-03-29 14:55:01 -0400

    Retry fetching errata_ids 3 times if returned IDs are blank
    There can sometimes be a delay between installing a package that
    triggers applicable errata and applicability being calculated on the
    server. To avoid this race condition, this will make a number of
    attempts to fetch errata before failing the test.

--- tests/test_content_client.py
+++ tests/test_content_client.py
@@ -104,8 +104,17 @@ def test_available_errata(katello_client, entities, api, os_release_major):
     katello_client.run('yum -y install walrus-0.71')
 
     host = foreman_host(katello_client, api)
-    errata = api.resource('errata').call('index', {'host_id': host['id']})
-    errata_ids = [erratum['errata_id'] for erratum in errata['results']]
+
+    retries = 0
+    while retries < 5:
+        retries += 1
+        errata = api.resource('errata').call('index', {'host_id': host['id']})
+        errata_ids = [erratum['errata_id'] for erratum in errata['results']]
+
+        if not errata_ids:
+            time.sleep(3)
+        else:
+            break
 
     assert entities['errata_id'] in errata_ids
 
